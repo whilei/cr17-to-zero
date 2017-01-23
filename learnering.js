210225b914d2588093e3b6ceeed78829d8c8b9a4
Mr. Is <isaac.ardis@gmail.com>
2017-01-23 15:08:54 +0100

    Use custom SCSS theming instead of extra CSS file.
    It's more complicated to use an extra CSS file when using Reveal to build the project
    as a single file (which we may want to do by way of Github Pages along the way), and
    the themes are set up for mods, which is pretttty nice.

--- learnering.js
+++ learnering.js
@@ -8,4 +8,7 @@ vp.id = "videoPlaceholder";
 
 // insert our placeholder for where the video will go
 // (placement and style attributes handled in css)
-document.body.insertBefore(vp, document.body.firstChild);
+// and I think we want to have this only inserted once, vs appended to each section(aka slide)
+var slides = document.getElementsByClassName("slides")[0]; // there should only be one anyway
+slides.insertBefore(vp, slides.firstChild);
+// document.body.insertBefore(vp, document.body.firstChild);
